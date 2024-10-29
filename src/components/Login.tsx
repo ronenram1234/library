cf689c2a510d0966456719351f75bfda2bc62fd5
Ronen Ram <ronenram1234@gmail.com>
2024-10-29 19:21:03 +0200

    create services and user interface

---
+++ src/components/Login.tsx
@@ -0,0 +1,15 @@
+import { FunctionComponent } from "react";
+
+interface LoginProps {
+    
+}
+ 
+const Login: FunctionComponent<LoginProps> = () => {
+    return ( <>
+    
+    <h1>login</h1>
+    
+    </> );
+}
+ 
+export default Login;
---
+++ src/interfaces/User.ts
@@ -0,0 +1,11 @@
+export interface User{
+ id?:number;
+ firstName?:string;
+ lastName?:string;
+ email: string;
+ pass:string;
+ 
+
+
+
+}
